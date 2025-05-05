06063e8d55161d7dd1b29952f809e0e08ba27733
Santiagoschzr <151122664+Santiagoschzr@users.noreply.github.com>
2025-05-04 22:11:30 -0600

    Desc

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,7 +34,7 @@ export default function Home() {
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
+      description="WorkSense Docs"
     >
       <HomepageHeader />
       <main>
